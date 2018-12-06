d9a41d3be37063a8fa51e39e8f89d17d9468a87c
Felipe Camargo Gruendemann <fcgruendemann@inf.ufpel.edu.br>
2018-12-06 02:16:09 -0200

    Adicionar arquivos de projeto.

---
+++ GameBreakout/game.cpp
@@ -0,0 +1,177 @@
+/*******************************************************************
+** This code is part of Breakout.
+**
+** Breakout is free software: you can redistribute it and/or modify
+** it under the terms of the CC BY 4.0 license as published by
+** Creative Commons, either version 4 of the License, or (at your
+** option) any later version.
+******************************************************************/
+#include "game.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+#include "game_object.h"
+#include "ball_object.h"
+
+
+// Game-related State data
+SpriteRenderer  *Renderer;
+GameObject      *Player;
+
+// Initial velocity of the Ball
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+// Radius of the ball object
+const GLfloat BALL_RADIUS = 12.5f;
+
+BallObject     *Ball;
+
+Game::Game(GLuint width, GLuint height)
+	: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+
+}
+
+Game::~Game()
+{
+	delete Renderer;
+	delete Player;
+}
+
+void Game::Init()
+{
+	// Load shaders
+	ResourceManager::LoadShader("shaders/sprite.vs", "shaders/sprite.frag", nullptr, "sprite");
+	// Configure shaders
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width), static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
+	ResourceManager::GetShader("sprite").Use().SetInteger("sprite", 0);
+	ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+	// Load textures
+	ResourceManager::LoadTexture("textures/background.jpg", GL_FALSE, "background");
+	ResourceManager::LoadTexture("textures/awesomeface.png", GL_TRUE, "face");
+	ResourceManager::LoadTexture("textures/block.png", GL_FALSE, "block");
+	ResourceManager::LoadTexture("textures/block_solid.png", GL_FALSE, "block_solid");
+	ResourceManager::LoadTexture("textures/paddle.png", true, "paddle");
+	// Set render-specific controls
+	Shader myShader;
+	myShader = ResourceManager::GetShader("sprite");
+	Renderer = new SpriteRenderer(myShader);
+	// Load levels
+	GameLevel one; one.Load("levels/1.lvl", this->Width, this->Height * 0.5);
+	GameLevel two; two.Load("levels/2.lvl", this->Width, this->Height * 0.5);
+	GameLevel three; three.Load("levels/3.lvl", this->Width, this->Height * 0.5);
+	GameLevel four; four.Load("levels/4.lvl", this->Width, this->Height * 0.5);
+	this->Levels.push_back(one);
+	this->Levels.push_back(two);
+	this->Levels.push_back(three);
+	this->Levels.push_back(four);
+	this->Level = 0;
+	
+	// Configure game objects
+	// Player
+	glm::vec2 playerPos = glm::vec2(this->Width / 2 - PLAYER_SIZE.x / 2, this->Height - PLAYER_SIZE.y);
+	Player = new GameObject(playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
+	// Ball
+	glm::vec2 ballPos = playerPos + glm::vec2(PLAYER_SIZE.x / 2 - BALL_RADIUS, -BALL_RADIUS * 2);
+	Ball = new BallObject(ballPos, BALL_RADIUS, INITIAL_BALL_VELOCITY, ResourceManager::GetTexture("face"));
+
+}
+
+void Game::Update(GLfloat dt)
+{
+	// Update objects
+	Ball->Move(dt, this->Width);
+	// Check for collisions
+	this->DoCollisions();
+}
+
+
+void Game::ProcessInput(GLfloat dt)
+{
+	if (this->State == GAME_ACTIVE)
+	{
+		GLfloat velocity = PLAYER_VELOCITY * dt;
+		// Move playerboard
+		if (this->Keys[GLFW_KEY_A])
+		{
+			if (Player->Position.x >= 0)
+			{
+				Player->Position.x -= velocity;
+				if (Ball->Stuck)
+					Ball->Position.x -= velocity;
+			}
+		}
+		if (this->Keys[GLFW_KEY_D])
+		{
+			if (Player->Position.x <= this->Width - Player->Size.x)
+			{
+				Player->Position.x += velocity;
+				if (Ball->Stuck)
+					Ball->Position.x += velocity;
+			}
+		}
+		if (this->Keys[GLFW_KEY_SPACE])
+			Ball->Stuck = false;
+	}
+}
+
+void Game::Render()
+{
+	if (this->State == GAME_ACTIVE)
+	{
+		// Draw background
+		Texture2D myTexture;
+		myTexture = ResourceManager::GetTexture("background");
+		Renderer->DrawSprite(myTexture, glm::vec2(0, 0), glm::vec2(this->Width, this->Height), 0.0f);
+		// Draw level
+		this->Levels[this->Level].Draw(*Renderer);
+		// Draw player
+		Player->Draw(*Renderer);
+		Ball->Draw(*Renderer);
+	}
+}
+
+void Game::DoCollisions()
+{
+	for (GameObject &box : this->Levels[this->Level].Bricks)
+	{
+		if (!box.Destroyed)
+		{
+			if (CheckCollision(*Ball, box))
+			{
+				if (!box.IsSolid)
+					box.Destroyed = GL_TRUE;
+			}
+		}
+	}
+}
+
+GLboolean Game::CheckCollision(GameObject & one, GameObject & two)
+{
+	// Collision x-axis?
+	bool collisionX = one.Position.x + one.Size.x >= two.Position.x &&
+		two.Position.x + two.Size.x >= one.Position.x;
+	// Collision y-axis?
+	bool collisionY = one.Position.y + one.Size.y >= two.Position.y &&
+		two.Position.y + two.Size.y >= one.Position.y;
+	// Collision only if on both axes
+	return collisionX && collisionY;
+}
+
+GLboolean Game::CheckCollision(BallObject & one, GameObject & two)
+{
+	// Get center point circle first 
+	glm::vec2 center(one.Position + one.Radius);
+	// Calculate AABB info (center, half-extents)
+	glm::vec2 aabb_half_extents(two.Size.x / 2, two.Size.y / 2);
+	glm::vec2 aabb_center(
+		two.Position.x + aabb_half_extents.x,
+		two.Position.y + aabb_half_extents.y
+	);
+	// Get difference vector between both centers
+	glm::vec2 difference = center - aabb_center;
+	glm::vec2 clamped = glm::clamp(difference, -aabb_half_extents, aabb_half_extents);
+	// Add clamped value to AABB_center and we get the value of box closest to circle
+	glm::vec2 closest = aabb_center + clamped;
+	// Retrieve vector between center circle and closest point AABB and check if length <= radius
+	difference = closest - center;
+	return glm::length(difference) < one.Radius;
+}
